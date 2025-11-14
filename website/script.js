3818af5f535a6f66048b759cf2f049bf06193472
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-14 22:30:04 +0545

    remove webpage

--- website/script.js
+++
@@ -1,8 +0,0 @@
-let textarea = document.getElementsByTagName("textarea")[0];
-let runButton = document.getElementsByTagName("button")[0];
-let outputWrapper = document.getElementById("output-wrapper");
-
-runButton.addEventListener("click", () => {
-    outputWrapper.innerHTML = textarea.value;
-    console.log(textarea.value);
-})
